affb908cf459235dcd13c40bead43b83a19a32e9
UdaraDewshan <dewudara@gmail.com>
2025-10-08 00:12:18 +0530

    Design Login Page

--- src/main/java/Controller/LoginPage_Controller.java
+++ src/main/java/Controller/LoginPage_Controller.java
@@ -1,4 +1,25 @@
 package Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LoginPage_Controller {
+
+    @FXML
+    private Button btnLogin;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    private TextField txtUsername;
+
+    @FXML
+    void btnLoginAction(ActionEvent event) {
+
+    }
+
 }
