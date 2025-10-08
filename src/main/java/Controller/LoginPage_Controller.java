ed04a00e420a3c4311e8a94a7af8ed8f2a0a4059
UdaraDewshan <dewudara@gmail.com>
2025-10-08 20:39:52 +0530

    Add Login Controller

--- src/main/java/Controller/LoginPage_Controller.java
+++ src/main/java/Controller/LoginPage_Controller.java
@@ -2,6 +2,7 @@ package Controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -19,7 +20,38 @@ public class LoginPage_Controller {
 
     @FXML
     void btnLoginAction(ActionEvent event) {
-
+        String userName = txtUsername.getText();
+        String password = txtPassword.getText();
+        if (!userName.isEmpty() && !password.isEmpty()){
+            if (userName.equals("udara")){
+                if(password.equals("1234")){
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setTitle("Success");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Login successfully!");
+                    alert.showAndWait();
+                    
+                }else{
+                    Alert alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setTitle("Error");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Error Password");
+                    alert.showAndWait();
+                }
+            }else{
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error");
+                alert.setHeaderText(null);
+                alert.setContentText("Error Name");
+                alert.showAndWait();
+            }
+        }else{
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText(null);
+            alert.setContentText("UserName Field or Password Field is Empty");
+            alert.showAndWait();
+        }
     }
 
 }
