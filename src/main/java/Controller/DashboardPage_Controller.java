f8074d5bcc42ea825a091077c4d51aaa466536c0
UdaraDewshan <dewudara@gmail.com>
2025-10-08 20:56:50 +0530

    Add Dashboard Page

---
+++ src/main/java/Controller/DashboardPage_Controller.java
@@ -0,0 +1,4 @@
+package Controller;
+
+public class DashboardPage_Controller {
+}
--- src/main/java/Controller/LoginPage_Controller.java
+++ src/main/java/Controller/LoginPage_Controller.java
@@ -2,10 +2,15 @@ package Controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginPage_Controller {
 
@@ -30,7 +35,17 @@ public class LoginPage_Controller {
                     alert.setHeaderText(null);
                     alert.setContentText("Login successfully!");
                     alert.showAndWait();
-                    
+
+                    Stage stage = new Stage();
+                    try {
+                        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/DashboardPage.fxml"))));
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                    Stage stage1 = (Stage) btnLogin.getScene().getWindow();
+                    stage1.close();
+                    stage.show();
+
                 }else{
                     Alert alert = new Alert(Alert.AlertType.ERROR);
                     alert.setTitle("Error");
