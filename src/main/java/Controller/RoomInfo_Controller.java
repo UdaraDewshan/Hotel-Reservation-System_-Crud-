7b6fc9ee1a3100228496f4add7c6513d56059729
UdaraDewshan <dewudara@gmail.com>
2025-10-09 09:44:20 +0530

    Add Component Room Page

--- src/main/java/Controller/RoomInfo_Controller.java
+++ src/main/java/Controller/RoomInfo_Controller.java
@@ -1,4 +1,79 @@
 package Controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class RoomInfo_Controller {
+
+    @FXML
+    private Button btnAdd;
+
+    @FXML
+    private Button btnBack;
+
+    @FXML
+    private Button btnClear;
+
+    @FXML
+    private Button btnDelete;
+
+    @FXML
+    private Button btnUpdate;
+
+    @FXML
+    private TableColumn<?, ?> colCity;
+
+    @FXML
+    private TableColumn<?, ?> colCustomerId;
+
+    @FXML
+    private TableColumn<?, ?> colName;
+
+    @FXML
+    private TableColumn<?, ?> colPhone;
+
+    @FXML
+    private TableView<?> tblCustomer;
+
+    @FXML
+    private TextField txtCity;
+
+    @FXML
+    private TextField txtCustomerId;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtPhone;
+
+    @FXML
+    void btnAddAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnBackAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnClearAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateAction(ActionEvent event) {
+
+    }
+
 }
