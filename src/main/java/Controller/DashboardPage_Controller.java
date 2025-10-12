c6d7ebe66a57b806e5a1dd214077e6cecd21d4fb
UdaraDewshan <dewudara@gmail.com>
2025-10-12 22:20:07 +0530

    Add Customer InfoDTO

--- src/main/java/Controller/DashboardPage_Controller.java
+++ src/main/java/Controller/DashboardPage_Controller.java
@@ -24,7 +24,7 @@ public class DashboardPage_Controller {
     void customerInfoAction(ActionEvent event) {
         Stage stage = new Stage();
         try {
-            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource(""))));
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/CustomerInfoPage.fxml"))));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
